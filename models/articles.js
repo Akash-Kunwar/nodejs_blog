8726755cfb5997d3d128da07ba88792e390e3515
Akash-Kunwar <48173287+Akash-Kunwar@users.noreply.github.com>
2020-04-19 15:10:34 +0530

    Initial Features

---
+++ models/articles.js
@@ -0,0 +1,19 @@
+var mongoose =require('mongoose')
+
+
+var articlSchema =mongoose.Schema({
+    title:{
+        type:String,
+        require:true
+    },
+    author:{
+        type:String,
+        require:true
+    },
+    body:{
+        type:String,
+        require:true
+    }
+});
+
+var Articles =module.exports=mongoose.model('Article',articlSchema);
